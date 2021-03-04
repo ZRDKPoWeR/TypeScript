87f1d86096c27b7a0f40702c841412a7691e178b
Sheetal Nandi <shkamat@microsoft.com>
2021-03-03 17:49:07 -0800

    Make `cleanPersistedProgram` option internal

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -5985,7 +5985,7 @@ namespace ts {
         incremental?: boolean;
         tsBuildInfoFile?: string;
         persistResolutions?: string;
-        cleanPersistedProgram?: boolean;
+        /*@internal*/ cleanPersistedProgram?: boolean;
         removeComments?: boolean;
         rootDir?: string;
         rootDirs?: string[];
